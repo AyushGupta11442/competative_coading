f28a1095b3e948f96752d29c3f6c7635a63aa535
ayush gupta <ayushgupta11442@gmail.com>
2023-10-09 00:06:11 +0530

    fake

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
+#include <cstdlib>
 #include <vector>
-#include <cctype>
-#include <cstring>
-#include <string>
+#include <iterator>
+#include < algorithm>
 using namespace std;
 
-vector<vector<int>> threeSum(vector<int>& nums) {
-    int pnt1 = 0;
-    int pnt2 = nums.size() - 1;
-    vector<vector<int>> toreturn;
-    int i = pnt1 + 1;
-    while (pnt1 < pnt2) {
-        if (i != pnt1 && i != pnt2 && nums[pnt1] + nums[pnt2] + nums[i] == 0) {
-            vector <int> temp = {};
-            temp.push_back(nums[pnt1]);
-            temp.push_back(nums[i]);
-            temp.push_back(nums[pnt2]);
-            toreturn.push_back(temp);
-
-        }
-        ++i;
-        if (i == pnt2)
-        {
-            pnt1++;
-            pnt2--;
-            i = pnt1 + 1;
-        }
-
-    }
-    return toreturn;
-}
-
 int main() {
-    vector<int> arr = { -1,0,1,2,-1,-4 };
-    for (vector<int>  a : threeSum(arr)) {
-        for (int ay : a) {
-            cout << ay;
-        }
-    }
+	int t;
+	cin >> t;
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+		vector<int> arr(b);
+		int totalsum = 0;
+		for (int i = 0; i < a; i++) {
+			int temp;
+			cin >> temp;
+			arr[i] = temp;
+			totalsum += temp;
+		}
+		if (totalsum < b) {
+			cout << 0 << endl;
+		}
+		else {
+			sort(arr.begin(), arr.end());
+			for (auto i : arr) {
+				cout << i;
+			}
+			int sum = 0;
+			int val = 0;
+			for (int i = 0; i < a; i++) {
+				val = arr[i];
+				cout << val;
+				sum += val;
+				if (totalsum - b  < sum) {
+					cout << val << endl;
+				}
+			}
+		}
+	}
+	return 0;
 }
