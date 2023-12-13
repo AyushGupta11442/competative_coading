ffa41e7652777ce0ef0272bc6d1aee7c4f3dc8db
AyushGupta11442 <ayushgupta11442@gmail.com>
2023-12-13 10:51:23 +0530

    fixed point done

---
+++ geekforgeek/easy/lovebaber/searchine/fixed point.cpp
@@ -0,0 +1,24 @@
+// https://practice.geeksforgeeks.org/problems/value-equal-to-index-value1330/1
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> valueEqualToIndex(int arr[], int n) {
+	vector<int> toreturn;
+	for (int i = 0; i < n; i++) {
+		if (i + 1 == arr[i]) {
+			toreturn.push_back(i+1);
+		}
+	}
+	return toreturn;
+}
+
+int main() {
+	int arr[] = { 15,2 ,45, 12, 7 };
+	vector<int> arrr = valueEqualToIndex(arr, 5);
+	for (int i = 0; i < arrr.size(); i++) {
+		cout << arrr[i] << " ";
+	}
+}
