05ba6b551930f96287825f7bbcf17a56de5ac16a
AyushGupta11442 <ayushgupta11442@gmail.com>
2023-12-14 14:36:14 +0530

    longest consicutive done

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,39 +1,38 @@
 #include<iostream>
-#include<string>
+#include<algorithm>
 #include<vector>
+
 using namespace std;
-
-
-string reverseWords(string s) {
-	string temp = "";
-	vector<string> dummystring = {};
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] == ' ' && temp.size() > 0) {
-			dummystring.push_back(temp);
-			temp = "";
-		}
-		else {
-			temp += s[i];
-		}
+int longestConsecutive(vector<int>& nums) {
+	if (nums.size() == 1) {
+		return 1;
 	}
-	string toreturn = "";
-	for (int i = dummystring.size() - 1; i >= 0; i--) {
-		if (i = (dummystring.size() - 1)) {
-			toreturn += dummystring[i];
-		}
-		else {
-			toreturn = toreturn + " " + dummystring[i];
-		}
+	else if (nums.size() == 0) {
+		return 0;
 	}
-	return toreturn;
+	else {
+		sort(nums.begin(), nums.end());
+		int maxsize = 0;
+		int toreturn = 0;
+		int toadd =0 ;
+		for (int i = 1; i < nums.size(); i++) {
+			if ((nums[i] == (nums[i - 1] + 1)) || ((nums[i] == (nums[i - 1])))) {
+				maxsize++;
+				if ((nums[i] == (nums[i - 1]))){
+					toadd++;
+				}
+			}
+			else {
+				toreturn = max(maxsize, toreturn);
+				maxsize = 0;
+			}
+		}
+		toreturn = max(maxsize, toreturn);
+		return (toadd == 0) ? toreturn + 1 : toreturn;
+	}
 }
 
-
-
 int main() {
-	string str = "a good   example";
-	string prints = reverseWords(str);
-	for (char a : prints) {
-		cout << prints;
-	}
+	vector<int> arr = {0,0};
+	cout << longestConsecutive(arr);
 }
