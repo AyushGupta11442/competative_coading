22e801bbc0a99be91c66e02bc95e55b50aec478b
AyushGupta11442 <ayushgupta11442@gmail.com>
2024-09-17 22:51:13 +0530

    hti si sfake commit

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
-#include<set>
-#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
 using namespace std;
+int lengthOfLongestSubstring(string s) {
+	set<char> sets;
+	int result = 0;
+	for (int i = 0; i < s.size(); i++) {
+		if (sets.find(s[i]) == sets.end()) {
+			sets.insert(s[i]);
+			result = (sets.size() > result) ? sets.size() : result;
+		}
+		else {
+			int temp = 0;
+			while (sets.find(s[i]) != sets.end() && temp < i) {
+				auto it = sets.find(s[temp]);
+				sets.erase(it);
+				temp++;
+			}
+			result = (sets.size() > result) ? sets.size() : result;
+		}
+	}
+	return result;
 
-bool check(string s) {
-    set<char> set;
-    for (char c : s) {
-        set.insert(c);
-    }
-    return s.size() == set.size();
+
 }
 
-int main() {
-    int year;
-    cin >> year;
-    string strYear = std::to_string(year);
-    do {
-        year++;
-        strYear = std::to_string(year);
-    } while (!check(strYear));
-    cout << strYear << endl;
+int  main() {
+	string s = "pwwkew";
+	cout << lengthOfLongestSubstring(s);
+
 }
